a7b4f4e974175e000fe75721c849298bacc799a0
fiqryq <fiqrychoerudin@student.telkomuniversity.ac.id>
2020-12-25 21:19:23 +0700

    set nav active

--- app/Views/user/index.php
+++ app/Views/user/index.php
@@ -38,10 +38,10 @@
     <nav aria-label="...">
         <ul class="pagination justify-content-center">
             <li class="page-item disabled">
-                <a class="page-link" href="#" tabindex="-1">Previous</a>
+                <a class="page-link active" href="#" tabindex="-1">Previous</a>
             </li>
             <li class="page-item"><a class="page-link" href="#">1</a></li>
-            <li class="page-item active">
+            <li class="page-item">
                 <a class="page-link" href="#">2 <span class="sr-only">(current)</span></a>
             </li>
             <li class="page-item"><a class="page-link" href="#">3</a></li>
