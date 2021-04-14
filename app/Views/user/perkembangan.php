a703e9c6ea9fb35bc9592fbe701965b0c6297229
fiqryq <fiqryqq@telkomuniversity.ac.id>
2021-04-14 13:12:33 +0700

    fitur pesan

--- app/Views/user/perkembangan.php
+++ app/Views/user/perkembangan.php
@@ -3,13 +3,6 @@
 
 <!-- content -->
 <div class="container-fluid">
-  <!-- Add button -->
-  <div class="d-sm-flex align-items-center justify-content-between mb-4">
-        <h1 class="h3 mb-0 text-gray-800"></h1>
-        <button class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm" data-toggle="modal" data-target="#tambahmodal" data-whatever="@mdo">
-            <i class="fas fa-plus fa-sm text-white-50">
-            </i>Tambah Anak</button>
-    </div>
 
     <div class="card shadow mb-4 mb-6">
         <div class="card-header py-3">
@@ -23,6 +16,7 @@
                     <tr>
                         <th>No</th>
                         <th>Nama Anak</th>
+                        <th>Tanggal lahir</th>
                         <th>Action</th>
                     </tr>
                 </thead>
@@ -35,6 +29,7 @@
                         <tr>
                             <td><?= $no++ ?></td>
                             <td><?= $data['nama_anak'] ?></td>
+                            <td><?= $data['tanggal_lahir'] ?></td>
                             <td>
                                 <a href="<?= base_url('user/detail/' . $id); ?>" class="btn btn-primary btn-circle">
                                     <i class="fas fa-eye"></i>
@@ -48,26 +43,6 @@
         </div>
     </div>
 </div>
-
-     <!-- Tambah Modal -->
-     <div class="modal fade" id="tambahmodal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel">Tambah Jadwal</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                    </div>
-                    <div class="modal-body">
-                        <!-- Content -->
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-
-<!-- End Modal -->
 
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
